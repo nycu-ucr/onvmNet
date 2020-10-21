9bf8c2814270e33c96e96fde79436b3468ba4ded
uduck <changhcs@cs.nctu.edu.tw>
2020-10-21 18:14:13 +0800

    The definition of send_pkt

--- conn.c
+++ conn.c
@@ -4,6 +4,9 @@
 #define NF_TAG "go-nf"
 #define SERVICE_ID_DIGIT 10
 
+static uint16_t packet_size = 64
+static uint8_t d_addr_bytes[ETHER_ADDR_LEN];
+
 //typedef int GoInt;
 //extern int Handler(struct rte_mbuf*, struct onvm_pkt_meta*, struct onvm_nf_local_ctx*);
 
@@ -34,5 +37,71 @@ int onvm_init(struct onvm_nf_local_ctx *nf_local_ctx, int serviceId) {
 }
 
 void onvm_send_pkt(char * buff, int service_id, struct onvm_nf_local_ctx * ctx) {
+	uint32_t i;
+	uint32_t pkts_generated;
+	struct rte_mempool *pktmbuf_pool;
+
+	pkts_generated = 0;
+	pktmbuf_pool = rte_mempool_lookup(PKTMBUF_POOL_NAME);
+	if (pktmbuf_pool == NULL) {
+		onvm_nflib_stop(nf_local_ctx);
+		rte_exit(EXIT_FAILURE, "Cannot find mbuf pool!\n");
+	}
+
+	// create a packet
+	printf("Creating 1 packet for sending\n");
+
+	struct onvm_pkt_meta *pmeta;
+	struct ether_hdr *ehdr;
+
+	struct rte_mbuf *pkt = rte_pktmbuf_alloc(pktmbuf_pool);
+	if (pkt == NULL) {
+		printf("Failed to allocate packets\n");
+		return -1;
+	}
+
+	/*set up ether header and set new packet size*/
+	ehdr = (struct ether_hdr *)rte_pktmbuf_append(pkt, packet_size);
+
+	/*using manager mac addr for source
+	 *using input string for dest addr
+	 */
+
+	if (onvm_get_macaddr(0, &ehdr->s_addr) == -1) {
+		onvm_get_fake_macaddr(&ehdr->s_addr);
+	}
+
+	for (i = 0; i < ETHER_ADDR_LEN; ++i) {
+		ehdr->d_addr.addr_bytes[i] = d_addr_bytes[i];
+	}
+
+	ehdr->ether_type = LOCAL_EXPERIMENTAL_ETHER;
+
+	// fill out the meta data of the packet
+	pmeta = onvm_get_pkt_meta(pkt);
+	pmeta->destination = service_ID;
+	pmeta->action = ONVM_NF_ACTION_TONF;
+	//pmeta->flags = ONVM_SET_BIT(0, SPEED_TESTER_BIT);
+	//pkt->hash.rss = i;
+	pkt->hash.rss = 0;
+	pkt->port = 0;
+
+	/* Copy the packet into the rte_mbuf data section */
+	rte_memcpy(rte_pktmbuf_mtod(pkt, char *), buff, sizeof(msg));
+	pkts_generated = 1;
+
+	// send out the generated packet
+	onvm_nflib_return_pkt(nf_local_ctx->nf, pkt);
+
+
+
+	/* Exit if packets were unexpectedly not created */
+	if (pkts_generated == 0) {
+		onvm_nflib_stop(nf_local_ctx);
+		rte_exit(EXIT_FAILURE, "Failed to create packets\n");
+	}
+
+	return 0;
+
 }
 
